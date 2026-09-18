603704aef417a15afd7ea8afb10f2f7708a0da72
NathanDSanta <natan.d.santa@gmail.com>
2026-09-18 14:08:58 +0200

    first commit

---
+++ include/dt-bindings/deck_key.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define DECK_KEY_1 0x01
+#define DECK_KEY_2 0x02
+#define DECK_KEY_3 0x03
+#define DECK_KEY_4 0x04
+#define DECK_KEY_5 0x05
+#define DECK_KEY_6 0x06
+#define DECK_KEY_7 0x07
+#define DECK_KEY_8 0x08
---
+++ src/hid/deck_key_hid.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+int deck_key_hid_send(
+    const uint8_t *data,
+    size_t length
+);
